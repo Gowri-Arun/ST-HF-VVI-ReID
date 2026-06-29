f5406c2442542ea384cd467b10a670c6d8fdbdae
Gowri-Arun <208835626+Gowri-Arun@users.noreply.github.com>
2026-06-30 02:07:50 +0530

    Repo cleanup

--- data/buptcampus.py
+++
@@ -1,7 +0,0 @@
-from .base_video_dataset import BaseVideoDataset
-
-
-class BUPTcampus(BaseVideoDataset):
-    def __init__(self, root, seq_len=8, transform=None, split="train"):
-        super().__init__(root, seq_len, transform)
-        self.split = split
